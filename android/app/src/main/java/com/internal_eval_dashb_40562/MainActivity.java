74b7f92c9fecbcfd953404ed97409a3dfcc929bf
Crowdbotics <team@crowdbotics.com>
2023-05-08 23:45:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/internal_eval_dashb_40562/MainActivity.java
@@ -0,0 +1,15 @@
+package com.internal_eval_dashb_40562;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "internal_eval_dashb_40562";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "internal-eval-dashb-40562",
+  url: "https://internal-eval-dashb-40562.botics.co",
+  api: "https://internal-eval-dashb-40562.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
